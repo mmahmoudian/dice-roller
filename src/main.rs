676684adb284c9805e875b4a29d9be223ad577db
Niklas Paulin <naklis@hotmail.com>
2023-03-06 13:27:59 +0200

    Initial Rust GUI implementation of the dice rolling scripts

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use iced::widget::{button, column, row, text};
+use iced::{Sandbox, Settings, Element};
+
+mod roll_logic;
+
+struct DiceRoller {
+    // should be the dice roll
+    value: i32,
+    result: String,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    IncrementPressed,
+    DecrementPressed,
+    Roll,
+    Extended,
+}
+
+impl Sandbox for DiceRoller {
+
+    type Message = Message;
+
+    fn new() -> Self {
+        Self {
+            value: 0,
+            result: String::from("")
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("Test dice roller")
+    }
+
+    fn view(self: &DiceRoller) -> Element<Message> {
+
+        // vertical layout
+        column![
+            //horizontal layout for buttons in first row
+            row![
+                // increment number of dice on press
+                button("+").on_press(Message::IncrementPressed),
+                // show number of dice to roll in between buttons
+                text(self.value).size(25),
+                // decrement number of dice
+                button("-").on_press(Message::DecrementPressed),
+                // Perform the roll
+                button("Roll").on_press(Message::Roll),
+                // Perform extended test
+                button("Extended").on_press(Message::Extended),
+            ].spacing(10),
+            // Roll result as next row
+            text(&self.result).size(25),
+        ].into()
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+            Message::Roll => {
+                let res = roll_logic::roll(self.value, 6);
+                self.result = roll_logic::simple_print(&res);
+            }
+            Message::Extended => {
+                let mut rounds = self.value;
+                let mut round_res = Vec::new();
+                while rounds > 0{
+                    let r = roll_logic::roll(rounds, 6);
+                    let res = roll_logic::simple_print(&r);
+                    round_res.push(res);
+                    rounds -= 1;
+                }
+                self.result = round_res.join("\n");
+            }
+        }
+    }
+}
+
+pub fn main() -> iced::Result {
+    DiceRoller::run(Settings::default())
+}
---
+++ src/roll_logic.rs
@@ -0,0 +1,76 @@
+use rand::prelude::*;
+//use std::io::Write;
+
+pub fn roll(n: i32, sides: i32) -> Vec<i32> {
+    let mut rng = rand::thread_rng();
+    let mut roll = vec![0; sides as usize];
+    for _i in 0..n {
+        let roll_result = rng.gen_range(1..(sides + 1));
+        roll[(roll_result - 1) as usize] += 1;
+    }
+
+    roll
+}
+
+pub fn simple_print(res: &Vec<i32>) -> String {
+    let hits = count_hits(res);
+    let misses = res[0];
+    let total: i32 = res.iter().sum();
+    let miss_percentage: f32 = res[0] as f32 / total as f32;
+    let diff = hits - misses;
+    let mut status: String = String::new();
+    
+    if miss_percentage >= 0.5 {
+        status = String::from("!");
+    }
+    
+    if diff <= 0 {
+        status += "!";        
+    }
+
+    return format!("{}/{}/{}\t\t{}", hits, misses, total, status)
+}
+
+fn count_hits(rolls: &Vec<i32>) -> i32 {
+    let mut count = 0;
+    for i in 4..6 {
+        count += rolls[i]
+    }
+    count
+}
+
+// fn prompt(name:&str) -> String {
+//     let mut line = String::new();
+//     print!("{}", name);
+//     std::io::stdout().flush().unwrap();
+//     std::io::stdin().read_line(&mut line).expect("Error: Could not read a line");
+ 
+//     return line.trim().to_string()
+// }
+
+
+// fn main() {
+//     let side: i32 = 6;
+//     loop {
+//         let num = prompt("Number of rolls OR exit: ");
+//         if num.eq("exit") {
+//             break;
+//         }
+        
+//         if num.contains("*") {
+//             let split_result: Vec<&str> = num.split("*").collect();
+//             let mut r = split_result[0].parse().unwrap();
+//             println!("{}/{}/{}\t{}", "h", "m", "tot", "note");
+//             while r > 0 {
+//                 let result = roll(r, side);
+//                 simple_print(&result);
+//                 r -= 1;
+//             }
+
+//         } else {
+//             let r: i32 = num.parse().unwrap();
+//             let result = roll(r, side);
+//             simple_print(&result);
+//         }
+//     }
+// }
